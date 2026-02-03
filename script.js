41b9164ff384227bbf93329eba61c7f4980d8b56
aryanraj-11 <aryanrajw011@gmail.com>
2026-02-03 20:15:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,39 @@ function moveButton() {
     noBtn.style.left = `${x}px`;
     noBtn.style.top = `${y}px`;
 }
-
-function showSurprise() {
+    function showSurprise() {
+    // Hide the question page and show the surprise
     document.getElementById('question-page').classList.add('hidden');
     document.getElementById('surprise-page').classList.remove('hidden');
+
+    // Create a heart confetti explosion
+    var duration = 5 * 1000;
+    var animationEnd = Date.now() + duration;
+    var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+    function randomInRange(min, max) {
+      return Math.random() * (max - min) + min;
+    }
+
+    var interval = setInterval(function() {
+      var timeLeft = animationEnd - Date.now();
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+
+      var particleCount = 50 * (timeLeft / duration);
+      
+      // This shoots confetti from two sides of the screen
+      confetti(Object.assign({}, defaults, { 
+        particleCount, 
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+        colors: ['#ff0000', '#ff69b4', '#ff1493'] // Red and pinks
+      }));
+      confetti(Object.assign({}, defaults, { 
+        particleCount, 
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+        colors: ['#ff0000', '#ff69b4', '#ff1493']
+      }));
+    }, 250);
 }
